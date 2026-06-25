77717c59df60fc4be5e389d896e940c1e9c50c4b
rfxvsl-lab <rfxvsl.lab@gmail.com>
2026-06-25 20:38:20 +0700

    chore: remove sensitive and scratch files from tracking

--- patch_invitation.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/InvitationPreview.tsx', 'utf8');
-content = content.split('rsvps={rsvps}\\n        embedded={embedded}').join('rsvps={rsvps}\n        embedded={embedded}');
-fs.writeFileSync('src/components/InvitationPreview.tsx', content);
